c60bccc38099504bd3f0643e6c021a80be082d8a
KimTuyen2409 <kimtuyen240904@gmail.com>
2025-03-05 10:34:50 +0700

    commit

---
+++ Day7.js
@@ -0,0 +1,14 @@
+
+// Declare scores for 3 subjects
+
+let mathScore = 8;
+let physicsScore = 7;
+let chemistryScore = 9;
+
+// Calculate average score
+let averageScore = ( mathSore + physicsScore + chemistryScore
+) / 3;
+
+// Print result
+
+console.log("Average score:", averageScore);
