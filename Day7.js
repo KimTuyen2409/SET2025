1396fa40a8b721a64c4781684a33d8469e000281
KimTuyen2409 <kimtuyen240904@gmail.com>
2025-04-28 21:50:47 +0700

    fixed day 7

--- Day7.js
+++ Day7.js
@@ -1,68 +1,81 @@
-// sự khác nhau giữa == và === là: == là so sánh giá trị, === là so sánh giá trị và kiểu dữ liệu. 
+// sự khác nhau giữa == và === là: == là so sánh giá trị, === là so sánh giá trị và kiểu dữ liệu.
 // Sự khác nhau giữa let, var và const là  const là giá trị không thay đổi, let là giá trị có thể thay đổi, Var giống let nhưng dễ gây lỗi nên hạn chế dùng.
-let weight = 50;
-let tall = 1.65;
-
-function calculateBMI(weight, tall) {
-  return weight / tall ** 2;
+let weight = 64;
+let height = 1.9;
+function BMI(weight, height) {
+  return weight / height ** 2;
 }
-console.log("Calculating BMI: " + calculateBMI(weight, tall));
+console.log("BMI: " + BMI(weight, height));
 
+let principal = 100000;
+let rate = 3.875;
+let time = 5;
 function calculateSimpleInterest(principal, rate, time) {
   return principal * (1 + rate * time);
 }
+console.log(
+  "Calculating Simple Interest: " +
+    calculateSimpleInterest(principal, rate, time)
+);
 
-function convertUSDToVND(amount) {
-  return amount * 25000;
+let USD = 10;
+function convertCurrency(USD) {
+  return USD * 25000;
 }
+console.log(USD + " USD = " + convertCurrency(USD) + " VND");
 
 function calculateTime(seconds) {
   let hours = 0;
   let minutes = 0;
-  if (seconds > 60) {
+  if (seconds >= 60) {
     minutes = Math.floor(seconds / 60);
     seconds = seconds - minutes * 60;
-    if (minutes > 60) {
-      hours = minutes / 60;
+    if (minutes >= 60) {
+      hours = Math.floor(minutes / 60);
       minutes = minutes - hours * 60;
     }
   }
-  return hours +":" + minutes +":" + seconds;
+  return `${hours}:${minutes}:${seconds}`;
 }
-console.log("Calculatimes: " + calculateTime(122));
+let seconds = 3 * 60 * 60 + 4 * 60 + 30;
+console.log(seconds + "seconds => " + calculateTime(seconds));
 
-
-// Execise 1: Calculate Average Score
 let mathScore = 8;
 let physicsScore = 7;
 let chemistryScore = 9;
+
 let averageScore = (mathScore + physicsScore + chemistryScore) / 3;
-console.log('Average score:', averageScore);
 
-// Execise 2: Calculate Rectangle Perimeter and Area
-let width = 5;
-let height = 3;
-let perimeter = 2 * (width + height);
-let area = width * height;
-console.log('Perimeter:', perimeter);
-console.log('Area:', area);
+console.log("Average score:", averageScore);
 
-// Execise 3: Convert Celsius to Fahrenheit
 let celsius = 30;
-let fahrenheit = celsius * 1.8 + 32;
-console.log(celsius + " (C) = " + fahrenheit + " (F)");
 
-// Execise 4: Calculate Tip Amount 
-let bill= 200000;
+let fahrenheit = (9 / 5) * celsius + 32;
+
+console.log(celsius + "°C = " + fahrenheit + "°F");
+
+let width = 5;
+height = 3;
+let perimeter = (width + height) * 2;
+
+let area = width * perimeter;
+
+console.log("Perimeter:", perimeter);
+console.log("Area:", area);
+
+let billAmount = 200000;
+
 let tipPercent = 10;
-let tipAmount = bill * tipPercent / 100;
-let totalAmount = bill + tipAmount;
-console.log('Bill amout:', bill);
-console.log('Tip amount:', tipAmount);
-console.log('Total amount:', totalAmount);
+let tipAmount = (billAmount * tipPercent) / 100;
 
-// Exercise 5: Check Even or Odd Number
+let totalAmount = tipAmount + billAmount;
+
+console.log("Bill amount:", billAmount);
+console.log("Tip amount:", tipAmount);
+console.log("Total amount:", totalAmount);
+
 let number = 7;
-let isEven = number % 2 === 0;
-isEven ? console.log(number + ' is an even number') : console.log(number + ' is an odd number');
 
+let isEven = number % 2 == 0;
+
+console.log("Number " + number + " is " + (isEven ? "even" : "odd"));
