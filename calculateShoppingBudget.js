20b17177d19c0852f2d790e68f7aba5fadc5a5fd
KimTuyen2409 <kimtuyen240904@gmail.com>
2025-04-30 00:30:06 +0700

    SET-260

---
+++ calculateShoppingBudget.js
@@ -0,0 +1,18 @@
+const list = [
+    [2, 1],
+    [1, 4],
+    [2, 2],
+  ];
+  
+  function calculateShoppingBudget(list) {
+    let sum = 0;
+    for (let i = 0; i < list.length; i++) {
+      let priceItem = 1;
+      for (let j = 0; j < list[0].length; j++) {
+        priceItem = list[i][j] * priceItem;
+      }
+      sum = sum + priceItem;
+      // sum = sum + list[i][0] * list[i][1];
+    }
+    return sum;
+  }
